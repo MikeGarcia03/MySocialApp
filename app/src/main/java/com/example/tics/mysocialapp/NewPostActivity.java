fbab724ea3298a65549bcbc20f6e55a177c0a4d8
mikegarcia03 <miguelangelgarciadesena@gmail.com>
2018-09-13 19:56:17 -0400

    There's a problem with getting the location.

--- app/src/main/java/com/example/tics/mysocialapp/NewPostActivity.java
+++ app/src/main/java/com/example/tics/mysocialapp/NewPostActivity.java
@@ -1,13 +1,121 @@
 package com.example.tics.mysocialapp;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.myhexaville.smartimagepicker.ImagePicker;
+import com.myhexaville.smartimagepicker.OnImagePickedListener;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class NewPostActivity extends AppCompatActivity {
+
+    private Location userLocation;
+    private LocationCallback locationCallback;
+    private LocationRequest locationRequest;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private Geocoder geocoder;
+    private ImagePicker imagePicker;
+    private Uri setectedUri;
+    private ImageButton imageSelected;
+    private TextView addressLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_post);
+        geocoder = new Geocoder(this, Locale.getDefault());
+        addressLocation = findViewById(R.id.postLocation);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        locationRequest = new LocationRequest();
+        locationCallback = new LocationCallback() {
+
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                if (locationResult != null) {
+                    userLocation = locationResult.getLocations().get(0);
+                    stopLocationUpdates();
+                    setAddressNameFromLocation();
+                }
+            }
+        };
+        initImagePicker();
+        startLocationUpdates();
+    }
+
+    private void setAddressNameFromLocation() {
+        try {
+            List<Address> addresses = new ArrayList<>(geocoder.getFromLocation(
+                    userLocation.getLatitude(),
+                    userLocation.getLongitude(),
+                    1));
+            addressLocation.setText(addresses.get(0).getAddressLine(0));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void startLocationUpdates() {
+        if(ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            return;
+        }
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
+    }
+
+    private void stopLocationUpdates() {
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+    }
+
+    private void initImagePicker() {
+        imageSelected = findViewById(R.id.imagePostImageButton);
+        imageSelected.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(imagePicker == null) {
+                    imagePicker = new ImagePicker(NewPostActivity.this, null, new OnImagePickedListener() {
+                        @Override
+                        public void onImagePicked(Uri imageUri) {
+                            setectedUri = imageUri;
+                            imageSelected.setImageURI(imageUri);
+                        }
+                    });
+                }
+                imagePicker.choosePicture(true);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        imagePicker.handleActivityResult(resultCode,requestCode, data);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        imagePicker.handlePermission(requestCode, grantResults);
     }
 }
