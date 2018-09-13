1cbf6ae8e536fdbb6e92f71d99dc3eb6e3746cfe
mikegarcia03 <miguelangelgarciadesena@gmail.com>
2018-09-12 20:08:08 -0400

    Login and sign up are working.

--- app/src/main/java/com/example/tics/mysocialapp/SignUpActivity.java
+++ app/src/main/java/com/example/tics/mysocialapp/SignUpActivity.java
@@ -1,6 +1,5 @@
 package com.example.tics.mysocialapp;
 
-import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,7 +12,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class LogInActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
     private EditText emailEtxt;
@@ -22,34 +21,26 @@ public class LogInActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_in);
+        setContentView(R.layout.activity_sign_up);
 
-        emailEtxt = findViewById(R.id.emailEditText);
-        passEtxt = findViewById(R.id.passwordEditText);
-        findViewById(R.id.loginButton).setOnClickListener(new View.OnClickListener() {
+        emailEtxt = findViewById(R.id.sEmailEditText);
+        passEtxt = findViewById(R.id.sPasswordEditText);
+        findViewById(R.id.sSignUpButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //TODO validate if user and pass are not empty
-                firebaseAuth.signInWithEmailAndPassword(emailEtxt.getText().toString(),
+                firebaseAuth.createUserWithEmailAndPassword(emailEtxt.getText().toString(),
                         passEtxt.getText().toString())
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
-                                if(task.isSuccessful()){
-                                    Toast.makeText(LogInActivity.this, "Process finished successful", Toast.LENGTH_LONG).show();
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(SignUpActivity.this, "Process finished successful", Toast.LENGTH_LONG).show();
                                 } else {
-                                    Toast.makeText(LogInActivity.this, "Pass o Usuario incorrecto", Toast.LENGTH_LONG).show();
+                                    Toast.makeText(SignUpActivity.this, "Does not work", Toast.LENGTH_LONG).show();
                                 }
                             }
                         });
-            }
-        });
-
-        findViewById(R.id.signupButton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(LogInActivity.this, SignUpActivity.class);
-                startActivity(i);
             }
         });
     }
